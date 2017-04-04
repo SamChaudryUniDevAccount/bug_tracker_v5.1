31bfdd042ff1e7d939f3eed8d0fb545e2994ede0
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-04-04 09:51:38 +0100

    Initial commit phpfiles only

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+include("dbconfig.php");
+
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // username and password sent from form
+
+//    echo "unsecured username is {$_POST['username']} and password is {$_POST['password']}";
+
+
+    $myusername = mysqli_real_escape_string($link,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($link,$_POST['password']);
+
+    //   echo "username is {$myusername} and password is {$mypassword}";
+
+    $sql = "SELECT * FROM user WHERE username = '$myusername' and passcode = '$mypassword'";
+    if ($result = mysqli_query($link,$sql))
+    {
+        $count = mysqli_num_rows($result);
+        echo $count;
+
+    }
+    else{
+        echo "ERROR Something not working!";
+    }
+    //$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    //$active = $row['active'];
+
+
+    if($count == 1) {
+
+        //session_register("myusername");
+
+        $_SESSION['login_user'] = $myusername;
+        echo "count is 1";
+        header('location: landing.php');
+
+    }else {
+
+        $error = "Your Login Name or Password is invalid";
+
+    }
+}
+else{
+
+
+    ?>
+
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+    <h1>It's Alive!!!</h1>
+
+    <form action="/" method="post">
+        <div class="imgcontainer">
+        </div>
+        <div class="container">
+            <label><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" required>
+
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" required>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+
+    </body>
+    </html>
+
+    <?php
+}
+?>
+
+
---
+++ landing.php
@@ -0,0 +1,109 @@
+<?php
+
+include('session.php');
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bug Tracker: Homepage</title>
+</head>
+<body >
+<div class="container-fluid">
+    <h3 class = "headerText "id="headerText modal-title"> Bug Tracker: Homepage</h3>
+    <div class="row">
+        <div class="panel panel-default col-lg-6">
+            <div class="panel-heading ">Fill out the form below with your issue</div>
+            <div class="panel-body ">
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label  class="col-sm-2 control-label labels">Name</label>
+                        <div class="col-sm-12">
+                            <input type="text" name ="Name" class=" inputData form-control" value="Sam Chaudry" >
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label  class="col-sm-2 control-label labels"> Department</label>
+                        <div class="col-sm-12">
+                            <input type="text"  name ="Department" class=" inputData form-control" value="IT" >
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label  class="col-sm-2 control-label labels"> Bug Id</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="BugID" class=" inputData form-control" value="123" >
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <p  class="col-sm-2 control-label labels">Priority</p>
+                        <div class="col-sm-12">
+                            <input type="text" name="Priority" class=" inputData form-control" value="High" >
+                        </div>
+                    </div>
+                </form>
+                <div class="form-group">
+                    <label for="comment">State your problem</label>
+                    <textarea id="problem-area" name ="Problem"class="form-control inputData" rows="10" id="problem" >
+                            JSON API not working please check and amend
+
+                        </textarea>
+                </div>
+                <label for="comment">Got a picture of the problem? Click the upload button to add it.</label>
+                <button id="upload" type="button" class="btn btn-success">Upload</button>
+                <br/>
+                <br/>
+                <button id="submitProblem" type="submit" class="btn btn-default btn-lg  btn-block">Submit issue</button>
+            </div>
+        </div>
+        <div class="panel panel-default col-lg-6">
+            <div class="panel-heading ">
+                <hp class="panel-title">Current Issues logged</hp>
+                <button id="refresh" type="button" class="btn btn-danger">Refresh</button>
+            </div>
+            <div class="panel-body ">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Department</th>
+                        <th>Bug Id</th>
+                        <th>Priority</th>
+                        <th>Issues</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>John</td>
+                        <td>IT</td>
+                        <td>1</td>
+                        <td>high</td>
+                        <td>Not Parsing JSON data</td>
+                    </tr>
+                    <tr>
+                        <td>John</td>
+                        <td>IT</td>
+                        <td>1</td>
+                        <td>high</td>
+                        <td>Not Parsing JSON data</td>
+                    </tr>
+                    <tr>
+                        <td>John</td>
+                        <td>IT</td>
+                        <td>1</td>
+                        <td>high</td>
+                        <td>Not Parsing JSON data</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
+
+?>
