d467d4fc97211314669f2bc1db9499234cb929b0
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-04-04 10:29:05 +0100

    Addition of  button onclick methods

--- Custom.js
+++ Custom.js
@@ -1,6 +1,55 @@
 //Working
 $( document ).ready(function() {
 
-    alert("JS Loaded in");
+    //alert("JS Loaded in");
 
 });
+
+$("#submitProblem").click(function(){
+
+    var jsonObject = {};
+
+    var inputDataArray = ($(".inputData").serializeArray());
+
+    for (var i = 0; i < inputDataArray.length; i++){
+
+        jsonObject[inputDataArray[i]['name']] = inputDataArray[i]['value'];
+
+    }
+
+
+    var data = JSON.stringify(jsonObject);
+
+
+    alert("" + data);
+
+
+    console.log(data);
+
+
+
+});
+
+
+
+/*
+
+Data Structure:
+
+ $.ajax({
+
+ "url": 'Services.php',
+ "type": 'POST',
+ "data": {"points" : data},
+ success: function(data) {
+
+ // Do something with data that came back.
+
+ alert(data);
+
+ }
+
+ });
+*/
+
+
