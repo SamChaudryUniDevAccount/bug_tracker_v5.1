c42354f61b5ddfac52ed2cd5deab658642bfded7
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-04-04 11:28:38 +0100

    Addition of for loop to get hrough the data and echo its outside with the  key value pairs extracted

--- services.php
+++ services.php
@@ -1,5 +1,9 @@
 <?php
 
+
+//{"Name":"Sam Chaudry","Department":"IT","BugID":"123","Priority":"High","Problem":" JSON API not working please check and amend"}
+
+
 include("dbconfig.php");
 
 //$link -> Database connection
@@ -8,6 +12,51 @@ if (isset($_POST["data"])) {
 
     $data = json_decode($_POST["data"],true);
 
-    echo "".$data;
+
+    $name;
+    $department;
+    $bugid;
+    $priority;
+    $problem;
+
+    foreach ($data as $key=>$value) {
+
+        if ($key == "Name") {
+
+            $name = $value;
+
+
+        }elseif ($key == "Department"){
+
+            $department =  $value;
+
+
+        }elseif ($key == "BugID"){
+
+            $bugid =  $value;
+
+
+        }elseif ($key == "Priority"){
+
+            $priority =  $value;
+
+
+        }elseif ($key == "Problem"){
+
+            $problem =  $value;
+
+
+        }
+
+    }
+
+        //Echo from outside the for loop
+        echo "From server...".$name.$department.$bugid.$priority.$problem;
+
+
+
+
+
+
 
 }
