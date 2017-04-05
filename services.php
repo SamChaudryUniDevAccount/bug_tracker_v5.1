fe9a8ece991ad19ca02c300db82bc83c0b9b209a
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-04-05 10:10:40 +0100

    Added back previous code that was working

--- services.php
+++ services.php
@@ -16,8 +16,59 @@ if (isset($_POST["data"])) {
 
     $data = json_decode($_POST["data"],true);
 
-    echo "Data has hit the backend....";
+
+    $name;
+    $department;
+    $bugid;
+    $priority;
+    $problem;
+
+    foreach ($data as $key=>$value) {
+
+        if ($key == "Name") {
+
+            $name = $value;
 
 
+        }elseif ($key == "Department"){
+
+            $department =  $value;
+
+
+        }elseif ($key == "BugID"){
+
+            $bugid =  $value;
+
+
+        }elseif ($key == "Priority"){
+
+            $priority =  $value;
+
+
+        }elseif ($key == "Problem"){
+
+            $problem =  $value;
+
+
+        }
+
+    }
+
+    //Echo from outside the for loop
+    echo "From server...".$name.$department.$bugid.$priority.$problem;
+
+
+    //Write to database and monitor response Table is called: bugs
+    //Columns: Name,Department,BugID,Priority,Problem
+
+    $sql = "INSERT INTO bugs (Name, Department, BugID, Priority,Problem) VALUES ('$name', '$department', '$bugid',$priority, '$problem')";
+
+    $result = $link->query($sql);
+
+    if($result > 0){
+
+        echo $result;
+
+    }
 
 }
