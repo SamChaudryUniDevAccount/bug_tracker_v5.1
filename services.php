64593109dbcbd2a4f106ad87a94210b6f2a515d8
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-04-05 09:56:33 +0100

    returnt to simple echo statement

--- services.php
+++ services.php
@@ -15,46 +15,5 @@ if (isset($_POST["data"])) {
     echo "Data has hit the backend....".$data;
 
 
-    $name;
-    $department;
-    $bugid;
-    $priority;
-    $problem;
-
-    foreach ($data as $key=>$value) {
-
-        if ($key == "Name") {
-
-            $name = $value;
-
-
-        }elseif ($key == "Department"){
-
-            $department =  $value;
-
-
-        }elseif ($key == "BugID"){
-
-            $bugid =  $value;
-
-
-        }elseif ($key == "Priority"){
-
-            $priority =  $value;
-
-
-        }elseif ($key == "Problem"){
-
-            $problem =  $value;
-
-
-        }
-
-    }
-
-        //Echo from outside the for loop
-        echo "From server...".$name.$department.$bugid.$priority.$problem;
-
-
 
 }
