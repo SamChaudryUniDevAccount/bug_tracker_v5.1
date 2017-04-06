bbb4c672354a313c9213528e1df7affc1a012af3
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-04-06 09:35:24 +0100

    Code changes to GET

--- servicesget.php
+++ servicesget.php
@@ -7,15 +7,29 @@ if(isset($_GET['getCurrentIssues'])){
 
     echo "Get called successful";
 
+    $sql= "SELECT * FROM bugs";
 
+    $result=mysql_query($sql);
 
+    if ($result){
 
+        $i=0;
 
+        $return =[];
 
+        while($row = mysql_fetch_array($result, MYSQL_NUM)){
+
+            $rows[] = $row;
+
+        }
+
+        echo json_encode($rows);
+
+    }else{
+
+        echo "ERROR";
+
+    }
 
 }
 
-/*
- *
- *
- */
