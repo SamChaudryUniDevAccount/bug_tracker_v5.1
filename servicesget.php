03d437d294a53fe567ce376096b11d47a0238deb
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-04-06 08:54:23 +0100

    Addiotn of GET Logic outside of the echo statement

--- servicesget.php
+++ servicesget.php
@@ -2,7 +2,30 @@
 
 if(isset($_GET['getCurrentIssues'])){
 
-echo "Get called successful";
+    echo "Get called successful";
+
+    $sql = "SELECT * FROM bugs";
+
+    //Empty array to hold results
+    $rows = array();
+
+    if(mysqli_query($link, $sql)){
+
+        while($r = mysql_fetch($sql)) {
+
+            $rows['currentIssues'][] = $r;
+
+
+        }
+
+    } else{
+
+        echo mysqli_error($link) . "ERROR: Could not able to execute $sql. ";
+
+    }
+
+
+    echo json_encode($rows);
 
 
 }
