de30bcd8cfb90bfc099bb2c7d812e8ed497af95f
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-04-09 13:33:15 +0100

    return to old code

--- landing.php
+++ landing.php
@@ -22,10 +22,7 @@ session_start();
 <body >
 <div class="container-fluid">
 
-    <div class="panel panel-success">
-        <div class="panel-heading">Panel with panel-success class</div>
-        <div class="panel-body">Panel Content</div>
-    </div>
+
 
     <a href="logout.php">Log out</a>
 
