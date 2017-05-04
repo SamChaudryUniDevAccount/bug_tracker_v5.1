203a92b756a1d7c5ea241a519b9246f336ce2a51
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-05-04 16:46:49 +0100

    removal of test service calls

--- servicesget.php
+++ servicesget.php
@@ -39,20 +39,8 @@ if(isset($_GET['getCurrentIssues'])){
     getIssuesLoggedWithTheirIDs();
 
 
-}elseif(isset($_GET['testAPICall'])){
-
-
-    testAPICall($test);
-
-
 }
 
-
-function testAPICall($test){
-
-    echo "hello".$test;
-
-}
 
 
 
